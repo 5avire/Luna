d6db8c578f9bc9cb120921e4aa26bc2c64a151f1
5avire <savonghimire@gmail.com>
2026-08-01 21:22:14 +0545

    Pinning and adding ImGui

--- Luna/src/Luna/Application.cpp
+++ Luna/src/Luna/Application.cpp
@@ -2,6 +2,8 @@
 
 #include "Luna/Log.h"
 
+#include <glad/glad.h>
+
 namespace Luna {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -43,6 +45,9 @@ namespace Luna {
     {
         while(m_Running)
         {
+            glClearColor(0.117f, 0.117f, 0.117f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
             for (Layer* layer : m_LayerStack)
                 layer->OnUpdate();
 
