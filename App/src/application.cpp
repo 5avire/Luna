bbbc18e157f4e950289dc2c2829d947eb3460aeb
Savvy <savonghimire@gmail.com>
2026-08-17 23:52:25 +0545

    Code Refactor

--- App/src/application.cpp
+++ App/src/application.cpp
@@ -2,15 +2,13 @@
 #include <glm/glm.hpp>
 #include <imgui/imgui.h>
 
-using namespace Luna;
-
-class ExampleLayer : public Layer
+class ExampleLayer : public Luna::Layer
 {
     public:
         ExampleLayer()
             : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f)
         {
-            m_VertexArray.reset(VertexArray::Create());
+            m_VertexArray.reset(Luna::VertexArray::Create());
 
             float vertices[3 * 7] = {
                 -0.5f, -0.5f, +0.0f, -0.0f, -0.0f, +0.5f, +1.0f,
@@ -18,23 +16,23 @@ class ExampleLayer : public Layer
                 +0.0f, +0.5f, +0.0f, +0.5f, +1.0f, +0.5f, +1.0f
             };
 
-            std::shared_ptr<VertexBuffer> vertexBuffer;
-            vertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+            std::shared_ptr<Luna::VertexBuffer> vertexBuffer;
+            vertexBuffer.reset(Luna::VertexBuffer::Create(vertices, sizeof(vertices)));
 
-            BufferLayout layout = {
-                { ShaderDataType::Float3, "a_Pos" },
-                { ShaderDataType::Float4, "a_Color" }
+            Luna::BufferLayout layout = {
+                { Luna::ShaderDataType::Float3, "a_Pos" },
+                { Luna::ShaderDataType::Float4, "a_Color" }
             };
             vertexBuffer->SetLayout(layout);
             m_VertexArray->AddVertexBuffer(vertexBuffer);
 
             uint32_t indices[3] = { 0, 1, 2 };
 
-            std::shared_ptr<IndexBuffer> indexBuffer;
-            indexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+            std::shared_ptr<Luna::IndexBuffer> indexBuffer;
+            indexBuffer.reset(Luna::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
             m_VertexArray->SetIndexBuffer(indexBuffer);
 
-            m_SqVertexArray.reset(VertexArray::Create());
+            m_SqVertexArray.reset(Luna::VertexArray::Create());
 
             float sqVertices[4 * 3] = {
                 -0.6f, -0.6f, +0.0f,
@@ -42,16 +40,16 @@ class ExampleLayer : public Layer
                 +0.6f, +0.6f, +0.0f,
                 -0.6f, +0.6f, +0.0f
             };
-            std::shared_ptr<VertexBuffer> squareVB(Luna::VertexBuffer::Create(sqVertices, sizeof(sqVertices)));
+            std::shared_ptr<Luna::VertexBuffer> squareVB(Luna::VertexBuffer::Create(sqVertices, sizeof(sqVertices)));
 
-            BufferLayout sqLayout = {
-                { ShaderDataType::Float3, "a_Pos" },
+            Luna::BufferLayout sqLayout = {
+                { Luna::ShaderDataType::Float3, "a_Pos" },
             };
             squareVB->SetLayout(sqLayout);
             m_SqVertexArray->AddVertexBuffer(squareVB);
 
             uint32_t sqIndices[6] = { 0, 1, 2, 2, 3, 0 };
-            std::shared_ptr<IndexBuffer> squareIB(Luna::IndexBuffer::Create(sqIndices, sizeof(sqIndices) / sizeof(uint32_t)));
+            std::shared_ptr<Luna::IndexBuffer> squareIB(Luna::IndexBuffer::Create(sqIndices, sizeof(sqIndices) / sizeof(uint32_t)));
             m_SqVertexArray->SetIndexBuffer(squareIB);
 
             std::string vertexShader = R"( 
@@ -82,7 +80,7 @@ class ExampleLayer : public Layer
                 }
             )";
 
-            m_Shader.reset(Shader::Create(vertexShader, fragmentShader));
+            m_Shader.reset(Luna::Shader::Create(vertexShader, fragmentShader));
 
             std::string sqVertexShader = R"( 
                 #version 460 core
@@ -108,26 +106,26 @@ class ExampleLayer : public Layer
                 }
             )";
 
-            m_ShaderSq.reset(Shader::Create(sqVertexShader, sqFragmentShader));
+            m_ShaderSq.reset(Luna::Shader::Create(sqVertexShader, sqFragmentShader));
         }
 
         void OnUpdate() override
         {
-            RenderCommand::SetClearColor({0.15f, 0.15f, 0.15f, 1.00f});
-            RenderCommand::Clear();
+            Luna::RenderCommand::SetClearColor({0.15f, 0.15f, 0.15f, 1.00f});
+            Luna::RenderCommand::Clear();
 
             CameraMovement();
 
             m_Camera.SetPosition(m_CameraPos);
             m_Camera.SetRotation(m_CameraRotation);
 
-            Renderer::BeginScene(m_Camera);
+            Luna::Renderer::BeginScene(m_Camera);
 
-            Renderer::Submit(m_ShaderSq, m_SqVertexArray);
+            Luna::Renderer::Submit(m_ShaderSq, m_SqVertexArray);
 
-            Renderer::Submit(m_Shader, m_VertexArray);
+            Luna::Renderer::Submit(m_Shader, m_VertexArray);
 
-            Renderer::EndScene();
+            Luna::Renderer::EndScene();
 
         }
 
@@ -138,42 +136,42 @@ class ExampleLayer : public Layer
             ImGui::End();
         }
 
-        void OnEvent(Event& event) override
+        void OnEvent(Luna::Event& event) override
         {
         }
     private:
         bool CameraMovement()
         {
-            if (Input::IsKeyPressed(LunaKey_S))
+            if (Luna::Input::IsKeyPressed(LunaKey_S))
                 m_CameraPos.y += 0.4f * 0.016f;
-            if (Input::IsKeyPressed(LunaKey_W))
+            if (Luna::Input::IsKeyPressed(LunaKey_W))
                 m_CameraPos.y -= 0.4f * 0.016f;
 
-            if (Input::IsKeyPressed(LunaKey_A))
+            if (Luna::Input::IsKeyPressed(LunaKey_A))
                 m_CameraPos.x += 0.4f * 0.016f;
-            if (Input::IsKeyPressed(LunaKey_D))
+            if (Luna::Input::IsKeyPressed(LunaKey_D))
                 m_CameraPos.x -= 0.4f * 0.016f;
 
-            if (Input::IsKeyPressed(LunaKey_E))
+            if (Luna::Input::IsKeyPressed(LunaKey_E))
                 m_CameraRotation += 15.0f * 0.016f;
-            if (Input::IsKeyPressed(LunaKey_Q))
+            if (Luna::Input::IsKeyPressed(LunaKey_Q))
                 m_CameraRotation -= 15.0f * 0.016f;
 
             return true;
         }
     private:
-        CameraOrtho m_Camera;
+        Luna::CameraOrtho m_Camera;
         glm::vec3 m_CameraPos = glm::vec3(0.0f);
         float m_CameraRotation = 0.0f;
 
-        std::shared_ptr<Shader> m_Shader;
-        std::shared_ptr<VertexArray> m_VertexArray;
+        std::shared_ptr<Luna::Shader> m_Shader;
+        std::shared_ptr<Luna::VertexArray> m_VertexArray;
 
-        std::shared_ptr<Shader> m_ShaderSq;
-        std::shared_ptr<VertexArray> m_SqVertexArray;
+        std::shared_ptr<Luna::Shader> m_ShaderSq;
+        std::shared_ptr<Luna::VertexArray> m_SqVertexArray;
 };
 
-class Sandbox : public Application
+class Sandbox : public Luna::Application
 {
     public:
         Sandbox()
@@ -186,7 +184,7 @@ class Sandbox : public Application
         }
 };
 
-Application* Luna::CreateApplication()
+Luna::Application* Luna::CreateApplication()
 {
     return new Sandbox();
 }
--- Luna/src/Luna.h
+++ Luna/src/Luna.h
@@ -1,34 +1,42 @@
 #pragma once
 
-// -- Core --
-#include <Luna/Core.h>
-
 // -- Application --
 #include <Luna/Application.h>
+// -- Application --
 
 // -- Application Layers --
 #include <Luna/Layer.h>
 #include <Luna/ImGui/ImGuiLayer.h>
+// -- Application Layers --
 
 // -- Inputs --
 #include <Luna/Input.h>
 #include <Luna/KeyCodes.h>
 #include <Luna/MouseButtonCodes.h>
+// -- Inputs --
 
 // -- Rendering --
 // -- API Stuff --
-#include <Luna/Renderer/Shader.h>
-#include <Luna/Renderer/Buffer.h>
 #include <Luna/Renderer/Renderer.h>
+#include <Luna/Renderer/RenderCommand.h>
+
+// -- Resources --
+#include <Luna/Renderer/Buffer.h>
+#include <Luna/Renderer/Shader.h>
 #include <Luna/Renderer/VertexArray.h>
 
 // -- MISC --
 #include <Luna/Renderer/Camera.h>
 
+// -- Rendering --
+
 // -- Logging ---
 // Bugs out without ostr sometimes
 #include <spdlog/fmt/ostr.h>
 #include <Luna/Log.h>
+// -- Logging ---
+
 
 // -- Entry Point --
 #include <Luna/EntryPoint.h>
+// -- Entry Point --
